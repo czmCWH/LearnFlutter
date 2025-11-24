230fbf52b216e577e6a01b5405525830b58d61ba
czm <zhengmao.chen@upai.com>
2025-11-24 17:16:12 +0800

    add

--- dart_learn/bin/dart_learn.dart
+++ dart_learn/bin/dart_learn.dart
@@ -3,7 +3,7 @@ import 'package:dart_learn/05-%E5%87%BD%E6%95%B0%E5%92%8C%E5%BC%82%E5%B8%B8/02-%
 
 
 
-import 'package:dart_learn/08-%E7%B1%BB/07-%E7%B1%BB%E7%9A%84%E4%BF%AE%E9%A5%B0%E7%AC%A6/05-Mixins%E6%B7%B7%E5%85%A5.dart' as dart_mixinss;
+import 'package:dart_learn/08-%E7%B1%BB/07-%E7%B1%BB%E7%9A%84%E4%BF%AE%E9%A5%B0%E7%AC%A6/05-Mixins%E6%B7%B7%E5%85%A5%E7%B1%BB.dart' as dart_mixinss;
 
 import 'package:dart_learn/10-并发/02-使用Future.dart' as dart_future;
 
